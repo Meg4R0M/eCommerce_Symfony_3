7ff6b81bdf02d5a27f218c5ed006289c701937e9
psykoterro <psykoterro@gmail.com>
2016-12-15 16:16:51 +0100

    Creation de la partie Admin (Gestion des pages OK)

---
+++ src/Pages/PagesBundle/Form/PagesType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pages\PagesBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PagesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre',TextType::class)
+            ->add('contenu',TextareaType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pages\PagesBundle\Entity\Pages'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'pages_pagesbundle_pages';
+    }
+
+
+}
