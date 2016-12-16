c68f35a0b1e12acddcd2ca55bb0230b5d5b7a1d3
meg4r0m <psykoterro@gmail.com>
2016-12-16 07:07:06 +0100

    Administration - Categories OK

---
+++ src/Ecommerce/EcommerceBundle/Form/CategoriesType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Ecommerce\EcommerceBundle\Form\MediaType;
+
+class CategoriesType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('image', MediaType::class)
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ecommerce\EcommerceBundle\Entity\Categories'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'ecommerce_ecommercebundle_categories';
+    }
+
+
+}
