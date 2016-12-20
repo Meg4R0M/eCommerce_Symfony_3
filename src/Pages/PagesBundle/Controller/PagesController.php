11751e0c72d196a1d6cb34bc3e9c4bb2f474149b
psykoterro <psykoterro@gmail.com>
2016-12-20 16:16:49 +0100

    Mise en place du formulaire de contact
    Creation du formulaire de contact complet, insertion en BDD et envoi de mail au gerant du site

--- src/Pages/PagesBundle/Controller/PagesController.php
+++ src/Pages/PagesBundle/Controller/PagesController.php
@@ -2,7 +2,10 @@
 
 namespace Pages\PagesBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Pages\PagesBundle\Entity\Enquiry;
+use Pages\PagesBundle\Form\EnquiryType;
 
 class PagesController extends Controller
 {
@@ -22,5 +25,36 @@ class PagesController extends Controller
         if (!$page) throw $this->createNotFoundException('La page n\'existe pas.');
 
         return $this->render('PagesBundle:Default:pages/layout/pages.html.twig', array('page' => $page));
+    }
+
+    public function contactAction(Request $request)
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($enquiry);
+            $em->flush();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Contact via Canturla Drive')
+                ->setFrom(array('psykotero@gmail.com' => "Canturla"))
+                ->setTo('psykoterro@gmail.com')
+                ->setCharset('utf-8')
+                ->setContentType('text/html')
+                ->setBody($this->renderView('PagesBundle:Default:pages/SwiftLayout/contactEmail.html.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+            $this->get('session')->getFlashBag()->Add('success', 'Votre message viens d\'être transmis, Merci a vous !');
+
+            return $this->redirect($this->generateUrl('contact'));
+        }
+
+        return $this->render('PagesBundle:Default:pages/layout/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
